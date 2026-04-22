c9c0bde1407582cea109033ee364a1b0e21c2e24
Josiah Obarhuakpor <josiahgare@gmail.com>
2026-04-22 04:05:07 -0500

    Update CustomerAddress.sql

--- scripts/CustomerAddress.sql
+++ scripts/CustomerAddress.sql
@@ -1,3 +1,13 @@
+/*
+=====================================
+Data Exploration of Customer Address
+=====================================
+Script Purpose:
+  This script explores the customer address dataset, checks for nulls and 
+  transforms the data as well
+
+*/
+
 -- preview of our table
 SELECT *
 FROM CustomerAddress;
